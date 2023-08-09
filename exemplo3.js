97fd1f05d2abf26415a7fc89dc0de2bdcce80b2f
josekbca <vilasboakbca@hotmail.com>
2023-08-08 21:38:30 -0300

    eita

---
+++ exemplo3.js
@@ -0,0 +1,16 @@
+function cadastrar(){
+    const nome = document.getElementById("nome").value; 
+    const idade = parseInt(document.getElementById("idade").value); 
+    const email = document.getElementById("email").value;
+    
+    const pessoa = {
+        nome: nome,
+        idade: idade,
+        email : email,
+    };
+
+    
+
+    document.getElementById("result").innerHTML = pessoa.nome + '<br>' + pessoa.idade + '<br>' + pessoa.email;
+
+}
