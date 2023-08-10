8095b5f96a66780b4c40808e09236bb2cde8351c
josekbca <vilasboakbca@hotmail.com>
2023-08-10 19:05:25 -0300

    aula33

--- exemplo3.js
+++ exemplo3.js
@@ -9,8 +9,24 @@ function cadastrar(){
         email : email,
     };
 
-    
+    const telefones= ['64 9 9999 9999', '64 9 9999 9991' ];
 
-    document.getElementById("result").innerHTML = pessoa.nome + '<br>' + pessoa.idade + '<br>' + pessoa.email;
+    const produtos = [
+        {nome: 'TV', quantidade: 10, preco: 2500.78},
+        {nome: 'celular', quantidade: 5, preco: 2900.45},
+        {nome: 'notebook', quantidade: 3, preco: 3500.89},
+        {nome: 'Computador', quantidade: 10, preco: 2871.45},
+    ];
+
+    document.getElementById("result").innerHTML = 
+    pessoa.nome +
+     '<br>' +
+      pessoa.idade +
+       '<br>' +
+        pessoa.email+
+        '<br>' +
+        telefones[0]+
+        '<br>' +
+        produtos[2].nome;
 
 }
